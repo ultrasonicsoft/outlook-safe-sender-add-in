5b174d15f07a16764aa4062ea1612745eaa1aa50
ultrasonicsoft <balramchavan@gmail.com>
2020-01-06 15:47:29 +0100

    displaying safe or not safe with different icon on perform action

---
+++ src/taskpane/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+const template = require("./app.component.html");
+/* global require */
+
+@Component({
+  selector: "app-home",
+  template
+})
+export default class AppComponent {
+  welcomeMessage = "Welcome";
+
+  async run() {
+    /**
+     * Insert your Outlook code here
+     */
+    console.log(Office.context.officeTheme.bodyBackgroundColor);
+  }
+}
